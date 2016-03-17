159dfa2cef75a7e60c45212c9100688bea9349e9
blazejchwiecko <blazej@vormlab.com>
2016-03-17 20:21:52 +0100

    animations added

--- AppleLogo/View/AnimationDot.swift
+++ AppleLogo/View/AnimationDot.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class AnimationDot: UIView {
 
-    var color       : UIColor?
     var position    : CGPoint?
     var radius      : CGFloat?
+    
+   // var path        = UIBezierPath()
 
     init(color: UIColor, position: CGPoint, radius: CGFloat) {
-        super.init(frame: CGRect(origin: position, size: CGSize(width: 2*radius, height: 2*radius)))
-        self.color = color
+        super.init(frame: CGRect(origin: CGPointMake(-1000, -1000), size: CGSize(width: 2*radius, height: 2*radius)))
         self.position = position
         self.radius = radius
+        backgroundColor = color
         self.layer.cornerRadius = radius
         self.clipsToBounds = true
     }
@@ -26,5 +27,33 @@ class AnimationDot: UIView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+//    func calculateBezierPath(view: UIView) {
+//        guard let position = position else { return }
+//        path.moveToPoint(CGPoint(x: position.x, y: view.frame.height))
+//        path.addLineToPoint(CGPoint(x: position.x, y: position.y))
+//        path.addLineToPoint(CGPoint(x: position.x, y: -view.frame.height))
+//    }
 
+    func animateViewMovment(view: UIView) {
+        guard let position = position else { return }
+        let animation = CABasicAnimation(keyPath: "position")
+        
+        let startPoint = CGPoint(x: position.x, y: view.frame.height)
+        let startPointObj = NSValue(CGPoint: startPoint)
+        animation.fromValue =  startPointObj
+        
+        let byPoint = CGPoint(x: position.x, y: position.y)
+        let byPointObj = NSValue(CGPoint: byPoint)
+        animation.byValue = byPointObj
+
+        let endPoint = CGPoint(x: position.x, y: -view.frame.height)
+        let endPointObj = NSValue(CGPoint: endPoint)
+        animation.toValue = endPointObj
+        animation.duration = 3
+        animation.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0.1, 0.25, 1)
+//        animation.values = [-667, 667/2, 667/2, 0]
+//        
+        self.layer.addAnimation(animation, forKey: "\(position.x)\(position.y)")
+    }
 }
--- AppleLogo/ViewController.swift
+++ AppleLogo/ViewController.swift
@@ -13,13 +13,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let animationDot = AnimationDot(color: UIColor.redColor(), position: CGPoint(x: 100, y: 100), radius: 20)
+        view.addSubview(animationDot)
+        //animationDot.calculateBezierPath(view)
+        animationDot.animateViewMovment(view)
+        let stopTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.5 * Double(NSEC_PER_SEC)))
+        dispatch_after(stopTime, dispatch_get_main_queue()) {
+            self.pauseLayer(animationDot.layer)
+        }
+        let resumeTime = dispatch_time(DISPATCH_TIME_NOW, Int64(2 * Double(NSEC_PER_SEC)))
+        dispatch_after(resumeTime, dispatch_get_main_queue()) {
+            self.resumeLayer(animationDot.layer)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func pauseLayer(layer: CALayer) {
+        let pausedTime = layer.convertTime(CACurrentMediaTime(), fromLayer: nil)
+        layer.speed = 0.0
+        layer.timeOffset = pausedTime
+    }
+    
+    func resumeLayer(layer: CALayer) {
+        let pausedTime = layer.timeOffset
+        layer.speed = 1.0
+        layer.timeOffset = 0.0
+        layer.beginTime = 0.0
+        let timeSincePause = layer.convertTime(CACurrentMediaTime(), fromLayer: nil) - pausedTime
+        layer.beginTime = timeSincePause
+    }
+    
 }
 
